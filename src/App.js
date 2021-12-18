9b53888a7cef82e5b8d35ff57d59753c6cb182f7
amkumar072 <amkumar072@gmail.com>
2021-12-18 12:56:13 +0000

    updated the folder structure

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import { Route, Switch } from "react-router-dom";
-import AllMeetup from "./component/AllMeetup";
+import AllMeetup from "./pages/AllMeetup";
 import FavouriteMeetup from "./component/FavouriteMeetup";
 import NewMeetup from "./component/NewMeetup";
 
